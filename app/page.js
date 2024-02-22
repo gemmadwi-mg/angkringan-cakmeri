fb6587b3f32f8414ef9fee45d8ef32fbba407b25
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-02-22 21:37:30 +0700

    finishing error

--- app/page.js
+++ app/page.js
@@ -2,19 +2,21 @@ import About from "@/components/About";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Map from "@/components/Map";
 import Menu from "@/components/Menu";
-import Image from "next/image";
+
+import dynamic from "next/dynamic";
+
+// Componente: HeroLocation
+const DynamicMap = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
-      //components
       <Header />
       <Hero />
       <Menu />
       <About />
-      <Map />
+      <DynamicMap />
       <Footer />
     </main>
   );
